6c92618dd1889f69e1cddea927577488248c97eb
ChrisAchinga <achinga.chris@gmail.com>
2020-04-08 23:57:09 +0300

    added apikey

---
+++ dist/js/script.js
@@ -0,0 +1,59 @@
+//Decalring the Different Variable and Objects
+let new_cases = document.getElementById("new_case");
+let new_death = document.getElementById("new_death");
+let total_death = document.getElementById("total_death");
+let total_recovered = document.getElementById("total_recovered");
+let total_cases = document.getElementById("total_cases");
+let table = document.getElementById('countries_stat')
+
+fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "ba61444e23msh2308b97d6e9dcbap10dd42jsne2697e354bcb" 
+    }
+})
+.then(response => response.json().then( data => {
+    console.log(data);
+    total_cases.innerHTML = data.total_cases;
+    new_cases.innerHTML = data.new_cases;
+    new_death.innerHTML = data.new_deaths;
+    total_death.innerHTML = data.total_deaths;
+    total_recovered.innerHTML = data.total_recovered;
+
+})).catch(err => {
+    console.log(err);
+});
+
+//Fetching The Case by Country Data
+fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "ba61444e23msh2308b97d6e9dcbap10dd42jsne2697e354bcb"
+    }
+})
+.then(response => response.json().then(data =>{
+    console.log(data)
+    let countries_stat = data.countries_stat;
+//Getting all the country statistic using a loop
+    for(let i = 0; i<countries_stat.length;i++){
+        console.log(countries_stat[i]);
+        //we will start by inserting the new rows inside our table
+        let row = table.insertRow(i+1);
+        let country_name = row.insertCell(0);
+        let cases = row.insertCell(1);
+        let deaths = row.insertCell(2);
+        let serious_critical = row.insertCell(3);
+        let recovered_per_country = row.insertCell(4);
+        country_name.innerHTML = countries_stat[i].country_name;
+        cases.innerHTML = countries_stat[i].cases;
+        deaths.innerHTML = countries_stat[i].deaths;
+        serious_critical.innerHTML = countries_stat[i].serious_critical;
+        recovered_per_country.innerHTML = countries_stat[i].total_recovered;
+
+    }
+}))
+.catch(err => {
+    console.log(err);
+});
